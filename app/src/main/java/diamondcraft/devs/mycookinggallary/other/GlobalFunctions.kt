69574d1bd1d803cce46de32b155ab8be1e1c4ea6
NIMESHBHAI MEHULBHAI VASANI <64627988+NimeshVasani@users.noreply.github.com>
2023-03-19 18:14:46 -0400

    new commit

--- app/src/main/java/diamondcraft/devs/mycookinggallary/other/GlobalFunctions.kt
+++ app/src/main/java/diamondcraft/devs/mycookinggallary/other/GlobalFunctions.kt
@@ -5,27 +5,22 @@ import java.text.Normalizer
 object GlobalFunctions {
     fun fromVulgarFraction(number1: String): Double {
         val number = number1.filterNot { it.isWhitespace() }
-        if (number == "1") {
-            return 1.0
 
-        } else {
-            val items = number.split("""\d""".toRegex()).filterNot { it.isEmpty() }
-            val mixed: String?
-            val fraction = mutableListOf<String>()
-            return if (items.isNotEmpty()) {
-                mixed = items.first()
-                fraction.addAll(Normalizer.normalize(mixed, Normalizer.Form.NFKC).split("\u2044"))
-                val decimal = fraction[0].toInt().toDouble() / fraction[1].toDouble()
-                val result = """\d+""".toRegex().find(number)
-                if (result != null) {
-                    result.value.toDouble() + decimal
-                } else {
-                    decimal
-                }
+        val items = number.split("""\d""".toRegex()).filterNot { it.isEmpty() }
+        val mixed: String?
+        val fraction = mutableListOf<String>()
+        return if (items.isNotEmpty()) {
+            mixed = items.first()
+            fraction.addAll(Normalizer.normalize(mixed, Normalizer.Form.NFKC).split("\u2044"))
+            val decimal = fraction[0].toInt().toDouble() / fraction[1].toDouble()
+            val result = """\d+""".toRegex().find(number)
+            if (result != null) {
+                result.value.toDouble() + decimal
             } else {
-                number.toDouble()
+                decimal
             }
+        } else {
+            number.toDouble()
         }
-
     }
 }
